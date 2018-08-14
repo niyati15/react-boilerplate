cc6284995966f2647a77e8e4e9613eeb59ecbae0
Niyati <niyati92@gmail.com>
2018-08-13 22:53:40 -0700

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const client = {
+    entry: {
+        'client': './src/client/index.js'
+    }
+};
+const server = {
+    entry: {
+        'server': './src/server/index.js'
+    }
+};
+module.exports = [client, server];
